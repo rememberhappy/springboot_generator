e7c20eadb8ad590a2bd43bfd8e7c66deec25a712
张鼎杰 <zhangdj@wdcloud.cc>
2022-06-12 13:12:37 +0800

    实体类生成 优化

--- src/main/java/cc/zdj/coder/generator/LombokPlugin.java
+++ src/main/java/cc/zdj/coder/generator/LombokPlugin.java
@@ -31,26 +31,31 @@ public class LombokPlugin extends PluginAdapter {
      */
     @Override
     public boolean modelBaseRecordClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
-        //实体类的import
+        // 实体类的import
 //        topLevelClass.addImportedType("java.io.Serializable");
         topLevelClass.addImportedType("lombok.Data");
-//        topLevelClass.addImportedType("lombok.Builder");
-//        topLevelClass.addImportedType("lombok.NoArgsConstructor");
-//        topLevelClass.addImportedType("lombok.AllArgsConstructor");
+        topLevelClass.addImportedType("lombok.Builder");
+        topLevelClass.addImportedType("lombok.NoArgsConstructor");
+        topLevelClass.addImportedType("lombok.AllArgsConstructor");
 
-        //实体类的注解
+        // 实体类的注解
+        // 单独使用 @Data 编译后会生成无参构造方法
         topLevelClass.addAnnotation("@Data");
-//        topLevelClass.addAnnotation("@Builder");
-//        topLevelClass.addAnnotation("@NoArgsConstructor");
-//        topLevelClass.addAnnotation("@AllArgsConstructor");
+        // 用来生成对象，并且可以为对象链式赋值。单独使用会生成 全属性参数的构造方法
+        // 注意 @Builder 注解会使 实体类中的默认值失效，需要在属性上添加注解保证 属性默认值的有效：@Builder.Default
+        // 使用方式：UserPopup userPopup = UserPopup.builder().createUserId(CommonInfoHolder.getUserId()).id(id).channelType(1).build();
+        topLevelClass.addAnnotation("@Builder");
+        // 如果直接使用 @Data和@Builder 两个注解，编译后会没有无参构造方法。如果此时需要添加无参构造注解：@NoArgsConstructor 会报错。解决办法，顺带添加 有参构造注解：@AllArgsConstructor
+        topLevelClass.addAnnotation("@NoArgsConstructor");
+        topLevelClass.addAnnotation("@AllArgsConstructor");
 
-        //实体类的注释
+        // 实体类上的注释
 //        topLevelClass.addJavaDocLine("/**");
 //        topLevelClass.addJavaDocLine(" * @author Sin");
 //        topLevelClass.addJavaDocLine(" * @date " + new SimpleDateFormat("yyyy/MM/dd/hh:mm").format(new Date()));
 //        topLevelClass.addJavaDocLine(" */");
 
-        //接口
+        // 实体类实现的接口
 //        topLevelClass.addSuperInterface(new FullyQualifiedJavaType("java.io.Serializable"));
         return true;
     }
